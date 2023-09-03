65ca6415f1c4e362c985b32df7989c630497eca5
hann <ejongwon7@naver.com>
2023-09-03 18:47:29 +0900

    ssch ver.0.24.5

--- ssch/python/logs.py
+++ ssch/python/logs.py
@@ -1,3 +1,4 @@
 def log(message, date):
     with open(f'/var/www/html/ssch/logs/{date}.log', mode='a', encoding='utf-8') as lg:
-        lg.write('\n'+message)
+        lg.write(message)
+        lg.write('\n\n============================================\n\n')
