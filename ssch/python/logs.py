99a44c994ee41bb49abe3a2417e427e42ff25310
hann <ejongwon7@naver.com>
2023-09-03 19:14:33 +0900

    ssch ver.0.24.6.7

--- ssch/python/logs.py
+++ ssch/python/logs.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+
+
 def log(message, date):
     with open(f'/var/www/html/ssch/logs/{date}.log', mode='a', encoding='utf-8') as lg:
+        lg.write(
+            f'\n\n\n==============={datetime.now().strftime("%H:%M:%S")}===============\n\n')
         lg.write(message)
-        lg.write('\n\n============================================\n\n')
