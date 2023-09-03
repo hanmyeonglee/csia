52ed4aa2900afdb7038788047ce062f1f0bbbd28
hann <ejongwon7@naver.com>
2023-09-03 19:02:31 +0900

    ssch ver.0.24.6.3

--- ssch/python/remote_sql_executer.py
+++ ssch/python/remote_sql_executer.py
@@ -1,8 +1,22 @@
 import pymysql
-mysql = pymysql.connect(user="login", passwd="DnKPnTZH5&dDDB%v", host="localhost",
-                        db="account", charset="utf8", autocommit=True)
+pw = {
+    "root": "todkagh",
+    "login": "DnKPnTZH5&dDDB%v",
+    "ssch": "rBXAm7WN"
+}
+db = {
+    "login": "account",
+    "ssch": "ssch"
+}
+id = input("id: ")
+if id == "root":
+    db[id] = input("db: ")
+mysql = pymysql.connect(user="login", passwd=pw[id], host="localhost",
+                        db=db[id], charset="utf8", autocommit=True)
 with mysql.cursor() as commander:
-    m = input("SQL: ")
-    commander.execute('%s', m)
+    n = int(input("sql number: "))
+    for _ in range(n):
+        m = input("SQL: ")
+        commander.execute('%s', m)
 
 mysql.close()
