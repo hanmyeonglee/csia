411fe05e858e26ddb77b0eb42db06b2a8e07a5c4
hann <ejongwon7@naver.com>
2023-09-03 18:58:24 +0900

    ssch ver.0.24.6.2

--- ssch/python/remote_sql_executer.py
+++ ssch/python/remote_sql_executer.py
@@ -2,7 +2,7 @@ import pymysql
 mysql = pymysql.connect(user="login", passwd="DnKPnTZH5&dDDB%v", host="localhost",
                         db="account", charset="utf8", autocommit=True)
 with mysql.cursor() as commander:
-    m = input("title: ")
-    commander.execute('update teacherAccount set title="%s"', m)
+    m = input("SQL: ")
+    commander.execute('%s', m)
 
 mysql.close()
