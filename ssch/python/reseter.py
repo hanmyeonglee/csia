e95dd40a7069131818673d6d6ee8d24c5d06b325
hann <ejongwon7@naver.com>
2023-09-03 18:57:15 +0900

    ssch ver.0.24.6.1

--- ssch/python/reseter.py
+++ ssch/python/reseter.py
@@ -2,7 +2,7 @@ import pymysql
 mysql = pymysql.connect(user="login", passwd="DnKPnTZH5&dDDB%v", host="localhost",
                         db="account", charset="utf8", autocommit=True)
 with mysql.cursor() as commander:
-    m = input()
+    m = input("title: ")
     commander.execute('update teacherAccount set title="%s"', m)
 
 mysql.close()
